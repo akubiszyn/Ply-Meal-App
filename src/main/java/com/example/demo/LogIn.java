9509546199def7146e1bb692f3b1784b73e9e9c4
Dominika <dominikaferfecka@email.com>
2023-01-04 23:19:24 +0100

    user login created

---
+++ src/main/java/com/example/demo/LogIn.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LogIn extends JFrame {
+    private JLabel usernameLabel;
+    private JTextField usernameTextField;
+    private JButton loginButton;
+    private JPanel panel1;
+
+    public LogIn() {
+        super();
+        this.setSize(300, 300);
+        this.setBounds(600, 250, 300, 300);
+        this.setContentPane(panel1);
+        DefaultListModel listModel = new DefaultListModel<>();
+        this.setVisible(true);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameTextField.getText();
+                System.out.println(username);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/demo/SingUp.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SingUp extends JFrame {
+    private JTextField usernameTextField;
+    private JPanel panel1;
+    private JButton singUpButton;
+    private JLabel usernameLabel;
+
+    public SingUp() {
+        super();
+        this.setSize(300, 300);
+        this.setBounds(600, 250, 300, 300);
+        this.setContentPane(panel1);
+        DefaultListModel listModel = new DefaultListModel<>();
+        this.setVisible(true);
+
+        singUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameTextField.getText();
+                System.out.println(username);
+            }
+        });
+    }
+}
